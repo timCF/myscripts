57f430f6de3dbe5a9849c2ac7728cf67c5a16193
tim <llStrainll@gmail.com>
2015-09-29 18:36:26 +0300

    some fixes

--- use_otp17.sh
+++ use_otp17.sh
@@ -3,5 +3,5 @@ PREFIX=~/OTP17
 PREFIXERL=$PREFIX/erl
 PREFIXEX=$PREFIX/elixir
 export PATH=$PREFIXERL/bin:$PREFIXEX/bin:$PATH
-#export PATH=~/OTP17/erl/bin:~/OTP17/elixir/bin:$PATH
 echo $PATH
+/bin/bash
